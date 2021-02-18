1514c2a9724525719ec5d6d723f7b4f25d958dbe
Cheng Pham <chengpham@gmail.com>
2021-02-18 10:40:21 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import AreaRechartComponent from './components/area.rechart';
+import LineRechartComponent from './components/line.rechart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [date, setDate] = useState(new Date())
+  const [date2, setDate2] = useState(new Date())
+  const [a, setA] = useState(true);
+
+  useEffect(() => {
+    const b = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    return () => {
+      clearInterval(b);
+    }
+  }, [])
+
+  useEffect(() => {
+    setDate2(new Date())
+  }, [a])
+
+  return(
+    <div>
+      <h2>Line Rechart</h2>
+      <LineRechartComponent />
+
+      <h2>Area Rechart</h2>
+      <AreaRechartComponent />
+
     </div>
-  );
+  )
 }
 
 export default App;
